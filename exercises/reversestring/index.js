839bd3cc1fafcbc2e2e4ba9c1a23909d20b5bee3
Andrew <andrew.vinocur@gmail.com>
2017-11-19 13:30:14 -0800

    first commit. includes first completed exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // V simplest solution V
+  // return str.split('').reverse().join('');
+
+  // Solution that doesnt use reverse() "cheat"
+  // var reverse = [];
+  // var split = str.split('');
+  // for (var i=str.length - 1; i > -1; i--){
+  //   reverse.push(split[i]);
+  // }
+  // var joined = reverse.join('');
+  // return joined;
+
+  // uses for/of syntax
+  // let reversed = '';
+  //
+  // for (let character of str){
+  //   reversed = character + reversed;
+  // }
+  //
+  // return reversed;
+
+  // demonstrates knowledge of ES2015. can be simplified more though
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
+}
 
 module.exports = reverse;
