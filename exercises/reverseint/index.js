c7e7bcb57aba97691932fb31a17b3bc7f005e7c3
Andrew <andrew.vinocur@gmail.com>
2017-11-20 17:39:10 -0800

    solved on my own, need to review grider's answer at later time

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  if (n === 0){
+    return 0;
+  } else {
+    if (Math.sign(n) === -1){
+      n = n * -1;
+      var arr = n.toString();
+      var soln = parseInt(arr.split('').reverse().join(''));
+      return soln * -1;
+    } else {
+      var arr = n.toString();
+      var soln = parseInt(arr.split('').reverse().join(''));
+      return soln;
+    }
+  }
+}
 
 module.exports = reverseInt;
