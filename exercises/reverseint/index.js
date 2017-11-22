136c87b5cbe1266ef9ab079e70a57a0344b87e4f
Andrew <andrew.vinocur@gmail.com>
2017-11-21 16:29:05 -0800

    simplest and DRY-est answer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,16 +9,8 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-  if (n === 0){
-    return 0;
-  } else {
-    var arr = parseInt(n.toString().split('').reverse().join(''));
-    if (Math.sign(n) === -1){
-      return arr * -1;
-    } else {
-      return arr;
-    }
-  }
+  var arr = parseInt(n.toString().split('').reverse().join(''));
+  return arr * Math.sign(n);
 }
 
 module.exports = reverseInt;
