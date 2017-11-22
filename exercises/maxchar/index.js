ec5cbcd79c2315285c25511fcd080cee6c8663b6
Andrew <andrew.vinocur@gmail.com>
2017-11-22 10:20:28 -0800

    returns char count as object correctly. figuring out how to find biggest

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,17 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  var count = {};
+  var arr = str.split('');
+  arr.forEach((char) => {
+    if (count.hasOwnProperty(char)){
+      count[char] = count[char] + 1;
+    } else {
+      count[char] = 1;
+    }
+  });
+  return count;
+}
 
 module.exports = maxChar;
