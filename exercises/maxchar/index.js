f9c69393f7b580c9045a4601e2dabc835ca9e77e
Andrew <andrew.vinocur@gmail.com>
2017-11-24 15:29:43 -0800

    comments out my soln to review griders. better syntax and avoids array

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,25 +6,52 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-  let count = {};
+// Grider's soln
+  const charMap = {};
   let max = 0;
-  let maxChar = '';
-  let arr = str.split('');
-  arr.forEach((char) => {
-    if (count.hasOwnProperty(char)){
-      count[char] = count[char] + 1;
-    } else {
-      count[char] = 1;
-    }
-  });
+  let macChar = '';
 
-  for (let char in count){
-    if (count[char] > max){
-      max = count[char];
+  for (let char of str){
+    if (charMap[char]){
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap){
+    if (charMap[char] > max){
+      max = charMap[char];
       maxChar = char;
     }
   }
+
   return maxChar;
+
+
+
+
+
+  // My original soln
+//   let count = {};
+//   let max = 0;
+//   let maxChar = '';
+//   let arr = str.split('');
+//   arr.forEach((char) => {
+//     if (count.hasOwnProperty(char)){
+//       count[char] = count[char] + 1;
+//     } else {
+//       count[char] = 1;
+//     }
+//   });
+//
+//   for (let char in count){
+//     if (count[char] > max){
+//       max = count[char];
+//       maxChar = char;
+//     }
+//   }
+//   return maxChar;
 }
 
 module.exports = maxChar;
