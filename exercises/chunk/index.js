5c34b2a0dec83c6bc64e10746b4da8c60207ce01
Andrew <andrew.vinocur@gmail.com>
2017-11-27 11:39:13 -0800

    grider's soln using SLICE

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -18,17 +18,30 @@ function chunk(array, size) {
 
 
   //Grider's Basic Soln
-  const chunked = [];
+  // const chunked = [];
+  //
+  // for (let element of array){
+  //   const last = chunked[chunked.length - 1];
+  //   if(!last || last.length === size){
+  //     chunked.push([element]);
+  //   } else {
+  //     last.push(element);
+  //   }
+  // }
+  // return chunked
 
-  for (let element of array){
-    const last = chunked[chunked.length - 1];
-    if(!last || last.length === size){
-      chunked.push([element]);
-    } else {
-      last.push(element);
-    }
+
+
+  //Grider's Slice Soln
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length){
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
-  return chunked
+
+  return chunked;
 }
 
 module.exports = chunk;
