f1a76e5ad2363efaf38decb1594689e8d4063e0c
Andrew <andrew.vinocur@gmail.com>
2017-12-01 12:23:50 -0800

    my initial soln. kinda ugly but it works

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,17 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+
+//my initial soln
+function steps(n) {
+  let init = 1;
+  while (init <= n) {
+    let a = '#'.repeat(init);
+    let b = ' '.repeat(n - init);
+    console.log(a + b);
+    init++;
+  }
+}
+
 
 module.exports = steps;
