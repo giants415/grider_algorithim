ba3984875492751176d07c935ed08db72132c1d6
Andrew <andrew.vinocur@gmail.com>
2017-12-01 12:05:43 -0800

    grider's 2nd, weaker, soln

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,14 +7,34 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-//Grider's 1st soln; very similar to mine but much DRYer so I've combined the 2 here
+//Grider's 2nd soln which identifies spaces for replacements
+// GRider considers it to be weak compared to his 1st
 function capitalize(str) {
-  const output = [];
-  for (let word of str.split(' ')){
-    output.push(word[0].toUpperCase() + word.slice(1));
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++){
+    if (str[i - 1] === ' ') {
+      result +=str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
   }
-  return output.join(' ');
+
+  return result;
 }
+
+
+
+
+
+//Grider's 1st soln; very similar to mine but much DRYer so I've combined the 2 here
+// function capitalize(str) {
+//   const output = [];
+//   for (let word of str.split(' ')){
+//     output.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return output.join(' ');
+// }
 
 
 
