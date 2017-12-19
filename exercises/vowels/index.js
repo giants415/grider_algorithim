3dc6bf92e2c1ffac07c6def643b07eb7d9dce9bf
Andrew <andrew.vinocur@gmail.com>
2017-12-19 15:00:57 -0800

    similar logic, much better syntax

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,17 +8,33 @@
 //   vowels('Why?') --> 0
 
 
-//my initial soln
-function vowels(str) {
-  const a = str.toLowerCase().split('');
-  let count = 0;
 
-  for (i = 0; i <= a.length; i++){
-    if (a[i] === 'a' || a[i] === 'e' || a[i] === 'i' || a[i] === 'o' || a[i] === 'u'){
+//Grider's iterative soln
+function vowels(str) {
+  let count = 0;
+  const checker = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let char of str.toLowerCase()){
+    if (checker.includes(char)){
       count++;
     }
   }
+
   return count;
 }
+
+
+//my initial soln
+// function vowels(str) {
+//   const a = str.toLowerCase().split('');
+//   let count = 0;
+//
+//   for (i = 0; i <= a.length; i++){
+//     if (a[i] === 'a' || a[i] === 'e' || a[i] === 'i' || a[i] === 'o' || a[i] === 'u'){
+//       count++;
+//     }
+//   }
+//   return count;
+// }
 
 module.exports = vowels;
